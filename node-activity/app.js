8c55628f585ab8148b18cc1eb75325a50c3d0c1e
Quouou <benedicto.christopheeer@gmail.com>
2022-12-02 19:55:36 +0800

    1st commit

---
+++ node-activity/app.js
@@ -0,0 +1,17 @@
+const { Console } = require('console');
+const http = require('http');
+const port = 4001
+
+const server = http.createServer(function(req,res){
+    res.write("Hello World")
+    res.end()
+})
+
+server.listen(port, function(error){
+    if(error){
+        console.log('Something went wrong', error)
+    }
+    else{
+        console.log('Server port is listening'+ port)
+    }
+})
